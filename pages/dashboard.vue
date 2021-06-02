c099df370ea1d0d6585431231ffe7f189e134518
Jerick-CM <jmangaluz@gmail.com>
2021-06-02 13:21:47 +0800

    fix dashboard menu

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container fluid class="grey ligthen-3  pa-0 ma-0" min-height="800vh">
     <v-sheet class="blue ligthen-3 pa-5 pt-10 pb-10" min-height="200vh">
-
-      <h1>Dashboard</h1>
-
-      <v-btn to="post/create">Create Post</v-btn>
-      <v-btn to="post/view">View Post</v-btn>
-
-
+      <v-card class="pa-2 ma-2">
+        <h1 class="black--text">Dashboard</h1>
+      </v-card>
+      <v-card class="pa-2 ma-2">
+        <h2>POST MENU</h2>
+        <v-btn to="post/create" class="green">Create Post</v-btn>
+        <v-btn to="post/view" class="blue">View Post</v-btn>
+      </v-card>
+      <v-card class="pa-2 ma-2">
+        <h2>NEWS MENU</h2>
+        <v-btn to="news/create" class="green">Create News</v-btn>
+        <v-btn to="news/view" class="blue">View News</v-btn>
+      </v-card>
     </v-sheet>
   </v-container>
 </template>
---
+++ pages/news/create.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-container fluid class="grey ligthen-3 pa-0 ma-0" min-height="800vh">
+    <v-sheet class="blue ligthen-3 pa-5 pt-10 pb-10" min-height="200vh">
+      <form class="white pa-5" action="">
+        <v-row
+          ><v-col>
+            <v-btn color="primary" depressed> POST </v-btn>
+          </v-col></v-row
+        >
+        <v-row>
+          <v-col>
+            <v-text-field v-model="form_title" label="Title"></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row
+          ><v-col>
+            <label for class="black--text">Content</label> <br />
+            <client-only placeholder="loading...">
+              <form action="">
+                <ckeditor-nuxt :config="editorConfig" v-model="form_content" />
+              </form> </client-only
+          ></v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn class="ml-0" color="success" v-on:click="onSubmit()">
+              <label class="white-font">Submit</label>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </form>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script>
+var url = process.env.BASE_URL_AXIOS;
+var timezone = process.env.TIMEZONE;
+console.log(url);
+export default {
+  middleware: "auth",
+
+  data() {
+    return {
+      url: null,
+      form_content: "",
+      form_title: "",
+      token: null,
+    };
+  },
+  components: {
+    "ckeditor-nuxt": () =>
+      import("@engrjerickcmangalus/ckeditor-nuxt-custom-build-simpleuploader"),
+  },
+  async created() {
+
+    this.$axios.$get("/sanctum/csrf-cookie").then((response) => {});
+    this.url = url;
+    this.timezone = timezone;
+    this.editorConfig = {
+      simpleUpload: {
+        uploadUrl: "http://back.api.test:3001/api/ckeditor",
+        withCredentials: true,
+        headers: {
+          Accept: "application/json",
+          Timezone: this.timezone,
+            // Authorization:"Bearer " + this.$auth.$storage.getCookies()["XSRF-TOKEN"],
+            "X-XSRF-TOKEN": this.$auth.$storage.getCookies()["XSRF-TOKEN"],
+            // "XSRF-TOKEN": this.$auth.$storage.getCookies()["XSRF-TOKEN"],
+            // "CSRF-TOKEN": this.$auth.$storage.getCookies()["XSRF-TOKEN"],
+        },
+      },
+    };
+  },
+  computed: {},
+  methods: {
+    onSubmit() {
+      console.log(this.$auth.user);
+      console.log(this.$store.state.auth.loggedIn);
+      console.log(this.$auth.$storage.getCookies());
+
+      var test = this.$auth.$storage.getCookies();
+      console.log("test");
+      // console.log(test.__gads);
+      console.log(test["XSRF-TOKEN"]);
+      // for (let key in test) {
+      //   console.log(key, test[key]);
+      // }
+      // console.log(this.$auth.strategy.refreshToken.get());
+      // Access using $auth
+      // this.$auth.loggedIn
+
+      // Access using vuex
+
+      if (this.form_title && this.form_content) {
+        // this.$axios.$get("/sanctum/csrf-cookie").then(response => {});
+        let payload = new FormData();
+
+        payload.append("title", this.form_title);
+        payload.append("content", this.form_content);
+        this.$axios
+          .post("/api/create-post", payload, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((res) => {})
+          .catch((error) => {})
+          .finally(() => {});
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+ul.clean {
+  list-style: none !important;
+  list-style-type: none !important;
+}
+</style>
