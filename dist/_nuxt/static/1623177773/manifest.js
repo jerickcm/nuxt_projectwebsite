6fae3a5b25e621df684c98a9ef9bff6d4c686a7c
Jerick C. Mangalus <jmangaluz@gmail.com>
2021-06-09 02:43:36 +0800

    test fix

---
+++ dist/_nuxt/static/1623177773/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog","\u002Fabout","\u002Findex84","\u002Fnews","\u002Fposts","\u002Fposts_view","\u002Fcontact-us","\u002Fregister","\u002Ftechnology","\u002Flogin","\u002Ftest","\u002Ftutorials"]})
