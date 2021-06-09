c7d5e5e90257224ecde3ffe87096f620acc11621
Jerick-CM <jmangaluz@gmail.com>
2021-06-09 16:30:47 +0800

    remove admin.js middleware and try to fix server fronend hosting

---
+++ dist/_nuxt/static/1623227357/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact-us","\u002Fadmin","\u002Fblog","\u002Findex84","\u002Fposts","\u002Fnews","\u002Flogin","\u002Fregister","\u002Ftechnology","\u002Ftutorials","\u002Flogout","\u002Ftest","\u002Fposts_view"]})
--- middleware/admin.js
+++
@@ -1,5 +0,0 @@
-// export default ({ app, redirect }) => {
-//   if (app.$auth.user['is_admin'] == 0) {
-//     return redirect('/')
-//   }
-// }
