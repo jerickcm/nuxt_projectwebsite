cab1945399e2f7004ef5b6ea5927fc90bcf49371
Jerick-CM <jmangaluz@gmail.com>
2021-06-10 13:50:53 +0800

    update

--- dist/_nuxt/static/1623254882/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontact-us","\u002Fblog","\u002Fadmin","\u002Fabout","\u002Flogin","\u002Flogout","\u002Fposts","\u002Ftest","\u002Fnews","\u002Fposts_view","\u002Fregister","\u002Ftechnology","\u002Fusers","\u002Ftutorials","\u002Findex84"]})
---
+++ dist/_nuxt/static/1623304077/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontact-us","\u002Fadmin","\u002Fabout","\u002Flogout","\u002Fblog","\u002Findex84","\u002Fregister","\u002Flogin","\u002Fposts","\u002Fnews","\u002Ftechnology","\u002Ftutorials","\u002Fusers","\u002Ftest","\u002Fposts_view"]})
