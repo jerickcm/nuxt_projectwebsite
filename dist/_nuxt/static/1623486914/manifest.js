7d7adc0b5ed9486fb09b65ff67b2bbdba423811b
Jerick-CM <jmangaluz@gmail.com>
2021-06-12 16:36:26 +0800

    fix domain signature

---
+++ dist/_nuxt/static/1623486914/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact-us","\u002Flogin","\u002Fadmin","\u002Fregister","\u002Flogout","\u002Ftechnology","\u002Fusers","\u002Ftemplate","\u002Ftutorials","\u002Fnews\u002Fview","\u002Ftest","\u002Fblog\u002Fview","\u002Fblog","\u002Fpost\u002Fview","\u002Fnews","\u002Fpost"]})
