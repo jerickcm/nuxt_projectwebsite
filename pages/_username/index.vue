6e694f3c2a06dbdda043e31e723a4927dd693f52
Jerick-CM <jmangaluz@gmail.com>
2021-06-18 16:25:26 +0800

    update profile information

--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    <h1>Profile # {{ id }}</h1>
-
-    <v-container>
-      <v-row> {{ profile }}</v-row>
-    </v-container>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Profile Username : {{ username }}</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-image :src="profile['details'].cover_photo"></v-image>
+          <v-image :src="profile['details'].profile_picture"></v-image>
+          <v-card-text>
+            <span>Name: {{ profile.name }}</span
+            ><br />
+            <span>Username: {{ username }}</span
+            ><br />
+            <span>Email: {{ profile.email }}</span
+            ><br />
+            <span> Address No: {{ profile['details'].house_number }} </span>
+            <br />
+            <span> Address street: {{ profile['details'].street }} </span>
+            <br />
+            <span> Address City: {{ profile['details'].city }} </span>
+            <br />
+            <span> Address province: {{ profile['details'].province }} </span>
+            <br />
+            <span> Address country: {{ profile['details'].country }} </span>
+            <br />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -13,7 +39,7 @@ export default {
   auth: false,
   head() {
     return {
-      title: 'Profile ' + this.id,
+      title: 'Profile ' + this.username,
       meta: [
         {
           hid: 'Profile',
@@ -31,13 +57,9 @@ export default {
     let response = await $axios.$get(
       `api/user_details/username/${params.username}`
     )
-    // console.log(response.user['details'])
     return { profile: response.user }
   },
   computed: {
-    id() {
-      return this.$route.params.username
-    },
     username() {
       return this.$route.params.username
     }
