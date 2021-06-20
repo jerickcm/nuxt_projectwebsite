5bffab74839c8cc290c8edfe7f551fc67d787516
Jerick-CM <jmangaluz@gmail.com>
2021-06-20 22:11:34 +0800

    fix

--- .nuxt/auth.js
+++ .nuxt/auth.js
@@ -53,13 +53,13 @@ export default function (ctx, inject) {
     "authorization": "https://facebook.com/v2.12/dialog/oauth",
     "userInfo": "https://graph.facebook.com/v6.0/me?fields=id,name,picture{url},email"
   },
-  "clientId": "134187988811148",
+  "clientId": "141292091328620",
   "name": "facebook"
 }))
 
   // github
   $auth.registerStrategy('github', new Oauth2Scheme($auth, {
-  "clientId": "9dc2d0876cb49c4b4027",
+  "clientId": "b916f0145cfe505852e1",
   "name": "github",
   "endpoints": {
     "authorization": "https://github.com/login/oauth/authorize",
@@ -97,7 +97,7 @@ export default function (ctx, inject) {
 
   // laravelSanctum
   $auth.registerStrategy('laravelSanctum', new CookieScheme($auth, {
-  "url": "http://localhost:3001",
+  "url": "https://backend.inhinyeru.com",
   "endpoints": {
     "csrf": {
       "withCredentials": true,
@@ -106,7 +106,7 @@ export default function (ctx, inject) {
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      "url": "http://localhost:3001/sanctum/csrf-cookie"
+      "url": "https://backend.inhinyeru.com/sanctum/csrf-cookie"
     },
     "login": {
       "withCredentials": true,
@@ -115,7 +115,7 @@ export default function (ctx, inject) {
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      "url": "http://localhost:3001/api/login"
+      "url": "https://backend.inhinyeru.com/api/login"
     },
     "logout": {
       "withCredentials": true,
@@ -124,7 +124,7 @@ export default function (ctx, inject) {
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      "url": "http://localhost:3001/api/logout"
+      "url": "https://backend.inhinyeru.com/api/logout"
     },
     "user": {
       "withCredentials": true,
@@ -133,7 +133,7 @@ export default function (ctx, inject) {
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      "url": "http://localhost:3001/api/user"
+      "url": "https://backend.inhinyeru.com/api/user"
     }
   },
   "name": "laravelSanctum",
---
+++ .nuxt/dist/server/pages/admin/index.js
@@ -0,0 +1,368 @@
+exports.ids = [16,2,3];
+exports.modules = {
+
+/***/ 205:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return VCardActions; });
+/* unused harmony export VCardSubtitle */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return VCardText; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return VCardTitle; });
+/* harmony import */ var _VCard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(206);
+/* harmony import */ var _util_helpers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(0);
+
+
+const VCardActions = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "i"])('v-card__actions');
+const VCardSubtitle = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "i"])('v-card__subtitle');
+const VCardText = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "i"])('v-card__text');
+const VCardTitle = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "i"])('v-card__title');
+
+/* unused harmony default export */ var _unused_webpack_default_export = ({
+  $_vuetify_subcomponents: {
+    VCard: _VCard__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"],
+    VCardActions,
+    VCardSubtitle,
+    VCardText,
+    VCardTitle
+  }
+});
+
+/***/ }),
+
+/***/ 252:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return admin; });
+const admin = {
+  middleware: 'admin',
+  layout: 'admin'
+};
+
+/***/ }),
+
+/***/ 281:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/DashTitle.vue?vue&type=template&id=78ce7fe9&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-card',{staticClass:"pa-2 ma-2"},[_c('v-card-title',[_c('v-btn',_vm._b({},'v-btn',_vm.$attrs,false),[_c('h1',{staticClass:"black--text"},[_vm._v(_vm._s(_vm.Dashboard))])])],1)],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/DashTitle.vue?vue&type=template&id=78ce7fe9&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/DashTitle.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var DashTitlevue_type_script_lang_js_ = ({
+  inheritAttrs: false,
+  // dont make parent v-card inherit place location of inheritance
+  props: ["Dashboard", "Redirect"]
+});
+// CONCATENATED MODULE: ./components/DashTitle.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_DashTitlevue_type_script_lang_js_ = (DashTitlevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(10);
+
+// EXTERNAL MODULE: ./node_modules/vuetify-loader/lib/runtime/installComponents.js
+var installComponents = __webpack_require__(11);
+var installComponents_default = /*#__PURE__*/__webpack_require__.n(installComponents);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VBtn/VBtn.js + 2 modules
+var VBtn = __webpack_require__(209);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VCard/VCard.js
+var VCard = __webpack_require__(206);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VCard/index.js
+var components_VCard = __webpack_require__(205);
+
+// CONCATENATED MODULE: ./components/DashTitle.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_DashTitlevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "26305d4a"
+  
+)
+
+/* harmony default export */ var DashTitle = __webpack_exports__["default"] = (component.exports);
+
+/* vuetify-loader */
+
+
+
+
+installComponents_default()(component, {VBtn: VBtn["a" /* default */],VCard: VCard["a" /* default */],VCardTitle: components_VCard["c" /* VCardTitle */]})
+
+
+/***/ }),
+
+/***/ 282:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Dashboard.vue?vue&type=template&id=1f949f06&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-card',{staticClass:"pa-2 ma-2"},[_c('v-card-title',[_vm._t("menu_name")],2),_vm._v(" "),_c('v-card-text',[_vm._t("create_menu"),_vm._v(" "),_vm._t("view_menu")],2)],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/Dashboard.vue?vue&type=template&id=1f949f06&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Dashboard.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var Dashboardvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./components/Dashboard.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_Dashboardvue_type_script_lang_js_ = (Dashboardvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(10);
+
+// EXTERNAL MODULE: ./node_modules/vuetify-loader/lib/runtime/installComponents.js
+var installComponents = __webpack_require__(11);
+var installComponents_default = /*#__PURE__*/__webpack_require__.n(installComponents);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VCard/VCard.js
+var VCard = __webpack_require__(206);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VCard/index.js
+var components_VCard = __webpack_require__(205);
+
+// CONCATENATED MODULE: ./components/Dashboard.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_Dashboardvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "d5f4f1ee"
+  
+)
+
+/* harmony default export */ var Dashboard = __webpack_exports__["default"] = (component.exports);
+
+/* vuetify-loader */
+
+
+
+
+installComponents_default()(component, {VCard: VCard["a" /* default */],VCardText: components_VCard["b" /* VCardText */],VCardTitle: components_VCard["c" /* VCardTitle */]})
+
+
+/***/ }),
+
+/***/ 447:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/admin/index.vue?vue&type=template&id=986507c4&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-container',{staticClass:"grey ligthen-3 pa-0 ma-0",attrs:{"fluid":"","min-height":"800vh"}},[_c('v-sheet',{staticClass:"blue ligthen-3 pa-5 pt-10 pb-10",attrs:{"min-height":"200vh"}},[_c('DashTitle',{attrs:{"Dashboard":"Dashboard","to":"/dashboard"}}),_vm._v(" "),_c('Dashboard',[_c('h2',{attrs:{"slot":"menu_name"},slot:"menu_name"},[_vm._v("POST MENU")]),_vm._v(" "),_c('v-btn',{staticClass:"green white--text",attrs:{"slot":"create_menu","to":"admin/post/create"},slot:"create_menu"},[_vm._v("Create Post")]),_vm._v(" "),_c('v-btn',{staticClass:"blue white--text",attrs:{"slot":"view_menu","to":"admin/post/manage"},slot:"view_menu"},[_vm._v("Manage Post")])],1),_vm._v(" "),_c('Dashboard',[_c('h2',{attrs:{"slot":"menu_name"},slot:"menu_name"},[_vm._v("BLOG MENU")]),_vm._v(" "),_c('v-btn',{staticClass:"green white--text",attrs:{"slot":"create_menu","to":"admin/blog/create"},slot:"create_menu"},[_vm._v("Create BLOG")]),_vm._v(" "),_c('v-btn',{staticClass:"blue white--text",attrs:{"slot":"view_menu","to":"admin/blog/manage"},slot:"view_menu"},[_vm._v("Manage BLOG")])],1),_vm._v(" "),_c('Dashboard',[_c('h2',{attrs:{"slot":"menu_name"},slot:"menu_name"},[_vm._v("NEWS MENU")]),_vm._v(" "),_c('v-btn',{staticClass:"green white--text",attrs:{"slot":"create_menu","to":"admin/news/create"},slot:"create_menu"},[_vm._v("Create NEWS")]),_vm._v(" "),_c('v-btn',{staticClass:"blue white--text",attrs:{"slot":"view_menu","to":"admin/news/manage"},slot:"view_menu"},[_vm._v("Manage NEWS")])],1),_vm._v(" "),_c('Dashboard',[_c('h2',{attrs:{"slot":"menu_name"},slot:"menu_name"},[_vm._v("QUOTES MENU")]),_vm._v(" "),_c('v-btn',{staticClass:"green white--text",attrs:{"slot":"create_menu","to":"admin/quotes/create"},slot:"create_menu"},[_vm._v("Create QUOTES")]),_vm._v(" "),_c('v-btn',{staticClass:"blue white--text",attrs:{"slot":"view_menu","to":"admin/quotes/manage"},slot:"view_menu"},[_vm._v("Manage QUOTES")])],1),_vm._v(" "),_c('Dashboard',[_c('h2',{attrs:{"slot":"menu_name"},slot:"menu_name"},[_vm._v("World Reminders MENU")]),_vm._v(" "),_c('v-btn',{staticClass:"green white--text",attrs:{"slot":"create_menu","to":"admin/world-reminders/create"},slot:"create_menu"},[_vm._v("Create World Reminders")]),_vm._v(" "),_c('v-btn',{staticClass:"blue white--text",attrs:{"slot":"view_menu","to":"admin/world-reminders/manage"},slot:"view_menu"},[_vm._v("Manage World Reminders")])],1)],1)],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/admin/index.vue?vue&type=template&id=986507c4&
+
+// EXTERNAL MODULE: ./mixins/admin_pages.js
+var admin_pages = __webpack_require__(252);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/admin/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ var adminvue_type_script_lang_js_ = ({
+  mixins: [admin_pages["a" /* admin */]],
+  head: () => ({
+    title: 'Dashboard'
+  }),
+  data: () => ({}),
+
+  async asyncData({
+    $axios,
+    error,
+    params,
+    $auth
+  }) {},
+
+  computed: {
+    email() {
+      return this.$auth.state['user'].email;
+    }
+
+  },
+
+  async created() {
+    console.log(this.$auth.state['user'].email);
+    console.log(this.$auth.state['strategy']);
+
+    if (this.$auth.state['strategy'] == 'google') {
+      await this.$axios.$get('/sanctum/csrf-cookie');
+      let payload = new FormData();
+      payload.append('email', this.$auth.state['user'].email);
+      payload.append('name', this.$auth.state['user'].name);
+      let response = await this.$axios.$post(`api/validate/account`, payload);
+      return {
+        profile: response.data
+      };
+    }
+  }
+
+});
+// CONCATENATED MODULE: ./pages/admin/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_adminvue_type_script_lang_js_ = (adminvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(10);
+
+// EXTERNAL MODULE: ./node_modules/vuetify-loader/lib/runtime/installComponents.js
+var installComponents = __webpack_require__(11);
+var installComponents_default = /*#__PURE__*/__webpack_require__.n(installComponents);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VBtn/VBtn.js + 2 modules
+var VBtn = __webpack_require__(209);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VGrid/VContainer.js
+var VContainer = __webpack_require__(221);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VSheet/VSheet.js
+var VSheet = __webpack_require__(15);
+
+// CONCATENATED MODULE: ./pages/admin/index.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_adminvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "e4f722bc"
+  
+)
+
+/* harmony default export */ var admin = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents_default()(component, {DashTitle: __webpack_require__(281).default,Dashboard: __webpack_require__(282).default})
+
+
+/* vuetify-loader */
+
+
+
+
+installComponents_default()(component, {VBtn: VBtn["a" /* default */],VContainer: VContainer["a" /* default */],VSheet: VSheet["a" /* default */]})
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
